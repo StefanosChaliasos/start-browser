7be65b8169dc28629b0ac723e5d123c556151938
StefanosChaliasos <t8140192@dias.aueb.gr>
2017-10-17 00:15:25 +0300

    Add the option to select browser

--- start-browser/core.py
+++ start-browser/core.py
@@ -1,29 +1,37 @@
 import webbrowser
 import json
-import platform
 import sys
+import argparse
 
-def find_system_platform():
-    return platform.system()
 
-def set_chrome_path(platform):
-    if platform == "Darwin":
-        return 'open -a /Applications/Google\ Chrome.app %s'
-    elif platform == "Linux":
-        return '/usr/bin/google-chrome %s'
-    elif platform == "Windows":
-        return 'C:\Program Files (x86)\Google\Chrome\Application\chrome.exe %s'    
-    sys.exit("Operating System not supported")
+def parse_arguments():
+    if not len(sys.argv) > 1:               # check if an argument had given
+        return False
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--browser', choices=[
+                        'chrome', 'firefox', 'safari',
+                        'opera', 'chromium', 'windows-default'])
+
+    args = parser.parse_args()
+    return args.browser
+
 
 def parse_bookmarks():
     with open('start-browser/bookmarks.json') as links_file:
         return json.load(links_file)
 
+
 def open_browser():
     links = parse_bookmarks()
-    platform = find_system_platform()
-    chrome_path = set_chrome_path(platform)
+
+    browser_selection = parse_arguments()
 
     for url in links["links"].values():
-        webbrowser.get(chrome_path).open(url)
-
+        if browser_selection:
+            try:
+                webbrowser.get(browser_selection).open(url)
+            except Exception as e:
+                print("Browser not found")
+                sys.exit()
+        else:
+            webbrowser.get().open(url)
