d156fc47f078f5c0a61c8613901f55543519fad3
StefanosChaliasos <t8140192@dias.aueb.gr>
2017-10-16 04:13:45 +0300

    Add support for linux and windows

--- start-browser/core.py
+++ start-browser/core.py
@@ -1,16 +1,28 @@
 import webbrowser
 import json
+import platform
+import sys
+
+def find_system_platform():
+    return platform.system()
+
+def set_chrome_path(platform):
+    if platform == "Darwin":
+        return 'open -a /Applications/Google\ Chrome.app %s'
+    elif platform == "Linux":
+        return '/usr/bin/google-chrome %s'
+    elif platform == "Windows":
+        return 'C:\Program Files (x86)\Google\Chrome\Application\chrome.exe %s'    
+    sys.exit("Operating System not supported")
 
 def parse_bookmarks():
     with open('start-browser/bookmarks.json') as links_file:
         return json.load(links_file)
 
 def open_browser():
-    
     links = parse_bookmarks()
-    
-    # MacOS
-    chrome_path = 'open -a /Applications/Google\ Chrome.app %s'
+    platform = find_system_platform()
+    chrome_path = set_chrome_path(platform)
 
     for url in links["links"].values():
         webbrowser.get(chrome_path).open(url)
