b198e787e3e525756082a355cdc99a2713382927
StefanosChaliasos <t8140192@dias.aueb.gr>
2017-11-01 10:08:51 +0200

    Fix path for bookmarks.json

--- executable.sh
+++ executable.sh
@@ -3,7 +3,9 @@ touch openbrowser
 chmod +rwx openbrowser
 exec 3<> openbrowser
     echo "#!/bin/bash" >&3
-    echo "python $startbrowserpath/startbrowser/">&3
+    #echo "python $startbrowserpath/startbrowser">&3
+    #echo "python $startbrowserpath/startbrowser $@">&3
+    echo "python $startbrowserpath/startbrowser --browser chrome">&3
 exec 3>&-
 
 sudo mv openbrowser /usr/local/bin/openbrowser
--- startbrowser/core.py
+++ startbrowser/core.py
@@ -2,7 +2,18 @@ import webbrowser
 import json
 import sys
 import argparse
+import platform
 from collections import OrderedDict
+import os
+
+
+browsers = { "chrome":{"Darwin":"open -a /Applications/Google\ Chrome.app %s",
+                       "Linux":"/usr/bin/google-chrome %s",
+                       "Windows":"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe %s"},
+             "firefox":{"Darwin":"",
+                        "Linux":"",
+                        "Windows":""}
+           }
 
 
 def parse_arguments():
@@ -17,11 +28,30 @@ def parse_arguments():
     return args.browser
 
 
+def find_bookmarks_absolute_path():
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    return dir_path + "/bookmarks.json" 
+
+
 def parse_bookmarks():
-    with open('startbrowser/bookmarks.json') as links_file:
+    bookmarks = find_bookmarks_absolute_path()
+    with open(bookmarks) as links_file:
         links = json.load(links_file, object_pairs_hook=OrderedDict)
         return links
         
+
+def find_platform():
+    return platform.system()
+
+
+def browser_not_open(url, browser):
+    platform = find_platform()
+    try:
+        path = browsers[browser][platform]
+        webbrowser.get(path).open(url)
+    except Exception as e:
+        print("Browser not found")
+        sys.exit()
 
 
 def open_browser():
@@ -31,9 +61,9 @@ def open_browser():
     for url in links["links"].values():
         if browser_selection:
             try:
-                webbrowser.get(browser_selection).open(url)
-            except Exception as e:
-                print("Browser not found")
-                sys.exit()
+                #webbrowser.get(browser_selection).open(url)
+                browser_not_open(url)
+            except:
+                browser_not_open(url, browser_selection)
         else:
             webbrowser.get().open(url)
